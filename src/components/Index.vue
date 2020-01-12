b30297cdd60c3bd0230bb3f3d369cc612db85aff
keith M <keithmchd48@gmail.com>
2020-01-12 17:26:13 +0530

    just before introducing firestore

---
+++ src/components/Index.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="index container">
+    <div class="card" v-for="smoothie in smoothies" :key="smoothie.id">
+      <div class="card-content">
+        <i class="material-icons delete" @click="deleteSmoothie(smoothie.id)">delete</i>
+        <h2 class="indigo-text">{{ smoothie.title }}</h2>
+        <ul class="ingredients">
+          <li v-for="(ing, index) in smoothie.ingredients" :key="index">
+            <span class="chip">{{ ing }}</span>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Index',
+  data () {
+    return {
+      smoothies: [
+        {id: 1, title: 'Kinsella brew', slug: 'kinsella-brew', ingredients: ['banana', 'milk', 'coffee']},
+        {id: 2, title: 'Evening mood', slug: 'evening-mood', ingredients: ['waffle', 'water', 'milk']}
+      ]
+    }
+  },
+  methods: {
+    deleteSmoothie (id) {
+      this.smoothies = this.smoothies.filter(sm => {
+        return sm.id !== id
+      })
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+  .index{
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    grid-gap: 30px;
+    margin-top: 60px;
+  }
+  .index h2{
+    font-size: 1.8em;
+    text-align: center;
+    margin-top: 0;
+  }
+  .index .ingredients{
+    margin: 30px auto;
+  }
+  .index .ingredients li{
+    display: inline-block;
+  }
+  .index .delete {
+    position: absolute;
+    top: 4px;
+    right: 4px;
+    cursor: pointer;
+    color: #aaa;
+    font-size: 1.4em;
+  }
+</style>
